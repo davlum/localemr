0e8b8fd33778ff9509a9b76006f8181759a68b1d
David Lum/./Affiliates/Samsung Electronics <david.lum91@gmail.com>
2020-05-10 23:39:02 -0400

    Add configuration from CLI and env

---
+++ localemr/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class Configuration:
+    def __init__(self, args):
+        fetch_from_s3 = os.environ.get('FETCH_FROM_S3', args.fetch_from_s3)
+        self.fetch_from_s3 = fetch_from_s3 if isinstance(fetch_from_s3, bool) else fetch_from_s3 == 'True'
+        convert_s3_to_local = os.environ.get('CONVERT_S3_TO_LOCAL', args.convert_s3_to_local)
+        self.convert_s3_to_local = convert_s3_to_local if isinstance(convert_s3_to_local, bool) else convert_s3_to_local == 'True'
+        # If local_dir not specified temporary directories will be used
+        self.local_dir = os.environ.get('LOCAL_DIR', args.local_dir)
+        self.livy_host = os.environ.get('LIVY_HOST', args.livy_host)
+        self.max_fetch_from_s3 = os.environ.get('MAX_FETCH_FROM_S3', args.max_fetch_from_s3)
+        self.s3_host = os.environ.get('S3_HOST', args.s3_host)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import moto.server as server
 from localemr.emr.models import FakeCluster
 from localemr.local_emr import read_task_queue
+from localemr.config import Configuration
 from multiprocessing import Process
+import argparse
 import sys
 
 
@@ -18,7 +20,24 @@ sys.modules['moto.emr.utils'] = __import__('localemr.emr.utils')
 
 
 if __name__ == "__main__":
-    reader_process = Process(target=read_task_queue, args=(FakeCluster.process_queue, FakeCluster.status_queue,))
+    parser = argparse.ArgumentParser(description="Local AWS EMR - A local service that imitates AWS EMR ")
+    parser.add_argument('-p', '--port', help='Port to run the service on', type=int, default=3000)
+    parser.add_argument("-H", "--host", type=str, help="Which host to bind", default="0.0.0.0")
+    fetch_help = 'Will fetch files with an S3 prefix. Requires authentication.'
+    parser.add_argument('-f', '--fetch-from-s3', help=fetch_help, action='store_true')
+    convert_help = 'Converts the any paths on a submitted step from S3 to local directories.'
+    parser.add_argument('-c', '--convert-s3-to-local', help=convert_help, action='store_true')
+    livy_host_help = 'The host where the Apache Livy server is running'
+    parser.add_argument('-l', '--livy-host', help=livy_host_help, default='http://livy:8998')
+    parser.add_argument('-s', '--s3-host', help='The host where the S3 endpoint is', default=None)
+    max_fetch_help = 'Max number of bytes to fetch from S3'
+    parser.add_argument('-m', '--max-fetch-from-s3', help=max_fetch_help, default=100000, type=int)
+    local_dir_help = 'Where to download the S3 files to. If empty then a temp dir is used.'
+    parser.add_argument('-d', '--local-dir', help=local_dir_help, default=None)
+
+    args = parser.parse_args()
+    conf = Configuration(args)
+    reader_process = Process(target=read_task_queue, args=(conf, FakeCluster.process_queue, FakeCluster.status_queue,))
     reader_process.daemon = True
     reader_process.start()
-    server.main(['emr', '-H', '0.0.0.0', '-p3000'])
+    server.main(['emr', '-H', args.host, '--port', str(args.port)])
