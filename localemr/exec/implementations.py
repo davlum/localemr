6cb9d1127024a2201b0bc260bce3ff0c9def208c
David Lum/./Affiliates/Samsung Electronics <david.lum91@gmail.com>
2020-09-14 17:23:37 -0400

    Remove old impls

--- localemr/exec/implementations.py
+++ localemr/exec/implementations.py
@@ -5,8 +5,3 @@ from localemr.config import Configuration
 def get_livylike_impl(config: Configuration):
     from localemr.exec.livylike.models import LivyLike
     return LivyLike(config)
-
-
-def get_subprocess_impl(config: Configuration):
-    from localemr.exec.subprocess.models import Subprocess
-    return Subprocess(config)
--- localemr/fork/implementations.py
+++ localemr/fork/implementations.py
@@ -7,8 +7,3 @@ def get_docker_impl(config: Configuration):
     from localemr.fork.docker.models import Docker
     docker_client = docker.DockerClient(base_url=config.docker_base_url)
     return Docker(config, docker_client)
-
-
-def get_wget_impl(config: Configuration):
-    from localemr.fork.wget.models import Wget
-    return Wget(config)
